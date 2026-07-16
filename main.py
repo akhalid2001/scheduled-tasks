59e08c79c702c4d20647ff4dff48d0a7ab1864d3
ak2001 <azrakhalid.zte@gmail.com>
2026-07-16 00:35:21 -0400

    Remove Old Project Files

--- main.py
+++ main.py
@@ -1,38 +1,80 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
+#Creating a weather/rain notifier app, that sends out an email
 
-
-from datetime import datetime
-import pandas
-import random
+import requests
 import smtplib
 import os
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+#use this online tool: https://jsonviewer.stack.hu/
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+#32.742,-81.688  Sylvania, GA
+#Wheaton, GA 41.864725, -88.111311
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+API_KEY = os.environ.get("API_KEY")
+MY_LAT = 41.864725 # Your latitude
+MY_LONG = -88.111311  # Your longitude
+API_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+COUNT = 4
+#https://api.openweathermap.org/data/2.5/forecast?lat=40.417286&lon=-82.907120&appid=f4924e152cf51d60bd124b355ea3aae6
+
+
+#constants for email generation
+GMAIL = "smtp.gmail.com"
+HOTMAIL = "smtp.live.com"
+YAHOO = "smtp.mail.yahoo.com"
+my_email = os.environ.get("MY_EMAIL1")
+password = os.environ.get("MY_EMAIL1_PASSWORD")
+yahoo = os.environ.get("YAHOO_EMAIL")
+
+
+parameters ={
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": API_KEY,
+    "cnt": COUNT
+}
+response = requests.get(url=API_ENDPOINT,params=parameters)
+response.raise_for_status() #if there is a problem, we should catch those too.
+print(response.status_code)
+weather_data = response.json()
+
+# print(weather_data)
+# print(type(weather_data))
+# print(weather_data.keys())
+
+
+will_rain = False
+for item in weather_data['list']:
+    condition_code = (item['weather'][0]['id'])
+    if condition_code < 700:
+        will_rain = True
+        print(condition_code)
+    else:
+        print(condition_code)
+
+if will_rain:
+    #print("Bring umbrella")
+    with smtplib.SMTP(GMAIL) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=yahoo,
+                            msg=f"Subject: Rain Notifier\n\nThe weather shows rain for today, please bring an umbrella☂️\n")
+
+
+    # client = Client(account_sid, auth_token)
+    # message = client.messages.create(
+    #     from_= '+17372324091',
+    #     body= "It's going to rain, bring an umbrella",
+    #     to= '+12832126918'
+    # )
+    # print(message.status)
+
+
+# message = client.messages.create(
+#   from_="whatsapp:TWILIO_WHATSAPP_NUMBER",
+#   body="It's going to rain today. Remember to bring an umbrella",
+#   to="whatsapp:YOUR_TWILIO_VERIFIED_NUMBER"
+# )
+
